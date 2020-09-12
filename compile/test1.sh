f94dd745c2983ff37bf79e36f38fa940a98b815f
Holly-Jiang <51194501050@stu.ecnu.edu.cn>
2020-09-12 11:21:33 +0800

    'l'

--- compile/test1.sh
+++ compile/test1.sh
@@ -3,10 +3,10 @@ g++ -std=c++11 /root/graph/new/quantum_mapping_a_-/src/main_FY_VF2_Zulehner.cpp 
 g++ -std=c++11 /root/graph/new/quantum_mapping_a_-/src/main_GQL.cpp  -o main_GQL
 g++ -std=c++11 /root/graph/new/quantum_mapping_a_-/src/main_OPT_VF2.cpp  -o main_OPT_VF2
 
-./main_Zulehner /root/graph/new/quantum_mapping_a_-/examples_result/4gt4-v0_79.qasm 4gt4-v0_79
+./main_Zulehner /root/graph/new/quantum_mapping_a_-/examples_result/clip_206.qasm clip_206
 /root/graph/new/SubgraphComparing/build/matching/SubgraphMatching.out -d /root/graph/new/SubgraphComparing/test/sample_dataset/test_case_1.graph -q /root/graph/new/VF2_mapping_opt/data/graphDB/Ex2.my -filter GQL -order GQL -engine LFTJ -num MAX
 java -jar /root/graph/new/VF2_mapping_opt/out/artifacts/VF2_deal_data_linux_jar/VF2.jar
 java -jar /root/graph/new/VF2_mapping_opt/out/artifacts/VF2_linux_jar/VF2.jar
-./main_OPT_VF2 /root/graph/new/quantum_mapping_a_-/examples_result/4gt4-v0_79.qasm 4gt4-v0_79 
-./main_FY_VF2_Zulehner /root/graph/new/quantum_mapping_a_-/examples_result/4gt4-v0_79.qasm 4gt4-v0_79 
-./main_GQL /root/graph/new/quantum_mapping_a_-/examples_result/4gt4-v0_79.qasm 4gt4-v0_79
+./main_OPT_VF2 /root/graph/new/quantum_mapping_a_-/examples_result/clip_206.qasm clip_206 
+./main_FY_VF2_Zulehner /root/graph/new/quantum_mapping_a_-/examples_result/clip_206.qasm clip_206 
+./main_GQL /root/graph/new/quantum_mapping_a_-/examples_result/clip_206.qasm clip_206 
