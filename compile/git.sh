3e60c601682d27dd27c3a7c2adc38411d4e1ca80
Holly-Jiang <51194501050@stu.ecnu.edu.cn>
2020-09-15 11:45:08 +0800

    l

--- compile/git.sh
+++ compile/git.sh
@@ -1,10 +1,6 @@
 
 /root/graph/new/SubgraphComparing/build/matching/SubgraphMatching.out -d /root/graph/new/SubgraphComparing/test/sample_dataset/test_case_1.graph -q /root/graph/new/quantum_mapping_a_-/pre_result/urf1_278 -filter GQL -order GQL -engine LFTJ -num MAX
 
-/root/graph/new/SubgraphComparing/build/matching/SubgraphMatching.out -d /root/graph/new/SubgraphComparing/test/sample_dataset/test_case_1.graph -q /root/graph/new/quantum_mapping_a_-/pre_result/urf2_152 -filter GQL -order GQL -engine LFTJ -num MAX
-
-/root/graph/new/SubgraphComparing/build/matching/SubgraphMatching.out -d /root/graph/new/SubgraphComparing/test/sample_dataset/test_case_1.graph -q /root/graph/new/quantum_mapping_a_-/pre_result/urf2_277 -filter GQL -order GQL -engine LFTJ -num MAX
-
 /root/graph/new/SubgraphComparing/build/matching/SubgraphMatching.out -d /root/graph/new/SubgraphComparing/test/sample_dataset/test_case_1.graph -q /root/graph/new/quantum_mapping_a_-/pre_result/urf5_280 -filter GQL -order GQL -engine LFTJ -num MAX
 
 /root/graph/new/SubgraphComparing/build/matching/SubgraphMatching.out -d /root/graph/new/SubgraphComparing/test/sample_dataset/test_case_1.graph -q /root/graph/new/quantum_mapping_a_-/pre_result/xor5_254 -filter GQL -order GQL -engine LFTJ -num MAX
